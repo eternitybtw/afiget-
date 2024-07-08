b6cc5bb80f1ef68d92510b8aafc93335343ea4d7
Илья С. <istef551@gmail.com>
2024-07-08 14:55:16 +0300

    Modified main.go

--- main.go
+++ main.go
@@ -7,20 +7,17 @@ import (
 
 func main() {
 	var a string
-	b := 0
-	c := 1
-	println("Введите номер N чисел Фибоначчи:")
-	_, err := fmt.Scanln(&a)
-	if err != nil {
-		return
-	}
+	x := 0
+	y := 1
+	fmt.Println("Введите количество чисел Фибоначчи, которое вы хотите увидеть")
+	fmt.Scan(&a)
 	n, e := strconv.Atoi(a)
 	if e != nil {
 		println("Ошибка ввода")
 		return
 	}
 	for i := 0; i < n+1; i++ {
-		print(b, " ")
-		b, c = c, b+c
+		print(x, " ")
+		x, y = y, x+y
 	}
 }
