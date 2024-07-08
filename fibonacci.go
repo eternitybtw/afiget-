e3197b2d9ed1b5024c8f0c410448ecb882e884ff
Илья С. <istef551@gmail.com>
2024-07-08 15:02:26 +0300

    Renamed main.go

--- fibonacci.go
+++ fibonacci.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Printf("Hello, World!")
+	var a string
+	x := 0
+	y := 1
+	fmt.Println("Введите количество чисел Фибоначчи, которое вы хотите увидеть")
+	fmt.Scan(&a)
+	n, e := strconv.Atoi(a)
+	if e != nil {
+		println("Ошибка ввода")
+		return
+	}
+	for i := 0; i < n+1; i++ {
+		print(x, " ")
+		x, y = y, x+y
+	}
 }
